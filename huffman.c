e651c1cea0dcd217228d2560539a7bb6ea507373
Eric_Augustin <ericaugustin@hotmail.com>
2019-05-08 10:25:07 -0300

    Update huffman.c
    Consegui acertar a arvore e criar a primeira etapa, uma letra em cada folha com sua respectiva frequencia, agora "só" falta agrupar a letras pela frequencia e gerar o modelo final da arvore

--- huffman.c
+++ huffman.c
@@ -3,11 +3,11 @@
 #include <locale.h>
 #define TAM 500
 
-typedef struct arvore{ // defini√ß√£o da struct
+typedef struct arvore{ // definiÁ„o da struct
 	char letra; // char
 	int freq;
-	struct arvore *direita; // endere√ßo irm√£os
-	struct arvore *esquerda; // endere√ßo filhos
+	struct arvore *direita; // endereÁo irm„os
+	struct arvore *esquerda; // endereÁo filhos
 } arvore;
 typedef struct alfa{ // cria estrutura da lista de indice
 	char letra;// cria a letra
@@ -17,14 +17,14 @@ typedef struct alfa{ // cria estrutura da lista de indice
 int cria_lista(alfa lista[],char letra, int n){ // insere letra na lista e acumula frequencia
 	int i,status=0;
 	for (i=0;i<n;i++){ // corre toda a lista
-		if (letra==lista[i].letra){ // verifica se o caracter √© igual a algum da lista
+		if (letra==lista[i].letra){ // verifica se o caracter È igual a algum da lista
 			status=1;// se for muda o status para 1
 			lista[i].freq++; // acumula frequencia
 		}
 	}
-	if (status==0){ // se status =0 ent√£o letra n√£o est√° na lista
+	if (status==0){ // se status =0 ent„o letra n„o est· na lista
 		lista[n].letra=letra;// inclui letra na lista
-		lista[n].freq=1; // coloca frequencia =1 pois √© primeira ocorrencoa
+		lista[n].freq=1; // coloca frequencia =1 pois È primeira ocorrencoa
 		n++;// incrementa tamanho da lista
 	}
 	return n; // retorna novo tamanho da lista
@@ -36,17 +36,27 @@ void imprime_lista(alfa lista[], int n){ // simplesmente imprime a lista
 	}
 }
 
-void troca(alfa *a, alfa *b){ // fun√ß√£o de troca para facilitar codigo do bubble sort
+void troca(alfa *a, alfa *b){ // funÁ„o de troca para facilitar codigo do bubble sort
 	alfa aux; // cria variavel auxiliar
 	aux=*a;// copia o conteudo de a em aux
-	*a=*b; // copia o conteudo de b no endere√ßo de a
-	*b=aux;// copia aux no endere√ßo de b
+	*a=*b; // copia o conteudo de b no endereÁo de a
+	*b=aux;// copia aux no endereÁo de b
 }
 void organiza_lista(alfa lista[], int n){ // bubble sort para organizar a lista por frequencia
 	int i,j;
-	for (i=0;i<n-1;i++){// percorre a lista at√© n-1
+	for (i=0;i<n-1;i++){// percorre a lista atÈ n-1
 		for (j=i;j<n;j++){// percorre a lista a partir de i
-			if (lista[i].freq>lista[j].freq){ // compara e se for maior troca
+			if (lista[i].freq<lista[j].freq){ // compara e se for maior troca
+				troca(&lista[i],&lista[j]); // troca
+			}
+		}
+	}
+}
+void organiza_arvore(arvore a[],alfa lista[], int n){ // bubble sort para organizar a lista por frequencia
+	int  i,j;
+	for (i=0;i<n-1;i++){// percorre a lista atÈ n-1
+		for (j=i;j<n;j++){// percorre a lista a partir de i
+			if (lista[i].freq<lista[j].freq){ // compara e se for maior troca
 				troca(&lista[i],&lista[j]); // troca
 			}
 		}
@@ -55,40 +65,59 @@ void organiza_lista(alfa lista[], int n){ // bubble sort para organizar a lista 
 int recebe_texto(char frase[],alfa lista[]){ // recebe o texto em forma de string e copia ele para a lista
 	int i=0; // inicializa contador
 	int n=0; // inicializa variavel de contagem de itens da lista
-	while(frase[i]!='\0'){ // enquanto a frase n√£o chega no fim...
-		n=cria_lista(lista,frase[i],n);	// envia cada caracter para a fun√ßao cria lista e recebe de volta o numero de itens na lista
+	while(frase[i]!='\0'){ // enquanto a frase n„o chega no fim...
+		n=cria_lista(lista,frase[i],n);	// envia cada caracter para a funÁao cria lista e recebe de volta o numero de itens na lista
 		i++; // icrementa contador
 	}
 	return n; // retorna o numero de itens da lista
 }
-
-void cria_arvore(arvore a[], alfa lista[], int n){ // em contru√ß√£o - ha duvidas de como fazere isso ainda
-	int i; // inicializa contador
-	for (i=0;i<n;i++){ // copia cada parte da lista para o folhas da arvore
-		a[i].letra=lista[i].letra; //  copia a letra
-		a[i].freq=lista[i].freq; // copia a frequencia
-		a[i].direita=NULL; // define o endere√ßo da proxima a direita como NULL por ser folha
-		a[i].esquerda=NULL; // define o endere√ßo da proxima a esquerda como NULL por ser folha
+void percorre_arvore(arvore *a, alfa lista[], int max ){ // funÁ„o para trasnferir todos os caracteres da lista para a arvore
+	int i;
+	for (i=0;i<max;i++){
+		cria_arvore(a,lista,i);
 	}
 }
-
+void cria_arvore(arvore *a[], alfa lista[], int i){ // em contruÁ„o - contruido como vetor, parece que funcionou, 
+	 						  	   					//todos os caracteres est„o alinhados a direita falta agora criar uma funÁ„o que junte 
+	 						  	   					//os pares e altere a arvore
+	if (*a==NULL) {
+		*a = (arvore*)malloc(sizeof(arvore)); // aloca a memoria para a arvore
+		(*(*a)).letra=lista[i].letra; //  copia a letra
+		(*(*a)).freq=lista[i].freq; // copia a frequencia
+		(*(*a)).direita=NULL; // define o endereÁo da proxima a direita como NULL por ser folha
+		(*(*a)).esquerda=NULL;
+	}
+	else {
+		cria_arvore(&(*(*a)).direita, lista,i); // chama a recursividade a direita para formar a lista inicial
+		}
+}
+void imprime(arvore *a,int n){ // funÁao para imprimir a arvore como ela est·, recebe a lista e o numero de nÛs
+	int i;
+	n++;
+	if (a != NULL) {
+      imprime (a->direita,n);
+      for (i=0;i<n;i++){
+      	printf(" ");
+	  }
+      printf("%c %d\n",(*a).letra,(*a).freq);
+      imprime (a->esquerda,n); 
+   }	
+}
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int i=0,j;
 	alfa lista[TAM]; // cria lista
-	arvore a[TAM]; // cria arvore - duvida se cria como vetor estatico ou dinamico...
-	char frase[TAM]="frase para testar a fun√ßao de copiar os caracteres para o lista\ntexto na outra linha pra verificar se o programa √© capaz de lidar com essa quest√£o\nfuncionando perfeitamente";
+	arvore *a; // cria arvore - duvida se cria como vetor estatico ou dinamico...
+	char frase[TAM]="frase para testar a funÁao de copiar os caracteres para o lista texto na outra linha pra verificar se o programa È capaz de lidar com essa quest„o funcionando perfeitamente";
 	printf("%s\n",frase);// imprime a frase de teste
 	i=recebe_texto(frase,lista);// transforma a string numa lista de letras e retorna o numero de letras
 //	imprime_lista(lista,i);// imprime a lista para conferir o funcionamento
 	organiza_lista(lista,i);// organiza a lista para testar
-	printf("\n");// da um espa√ßo
+	printf("\n");// da um espaÁo
 	imprime_lista(lista,i);// imprime novamente para ver se organizou
-	printf("Primeira vers√£o da arvore\n");// separa√ß√£o para verificar se est√° funcionando
-	cria_arvore(a,lista,i);// envia a lista para cria√ß√£o das folhas da arvore
-	for (j=0;j<i;j++){
-		printf("%c %d\n",a[j].letra,a[j].freq); // imprime as folhas da arvore
-	}
-	return 0;// retorna 0 para informar que n√£o houve erro
+	printf("Primeira vers„o da arvore\n");// separaÁ„o para verificar se est· funcionando
+	percorre_arvore(&a,lista,i);// envia a lista para criaÁ„o das folhas da arvore
+	imprime(&(a[0]),i);
+	return 0;// retorna 0 para informar que n„o houve erro
 }
